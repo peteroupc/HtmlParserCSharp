164f866a053547ee9aad225d6b85f670a0be44ba
Peter Occil <poccil14@gmail.com>
2025-01-16 12:59:54 -0500

    Minor code edits

--- src/com/upokecenter/util/QueryStringHelper.cs
+++ src/com/upokecenter/util/QueryStringHelper.cs
@@ -10,7 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace com.upokecenter.util {
+namespace com.upokecenter.util{
   public sealed class QueryStringHelper {
     private QueryStringHelper() {
     }
